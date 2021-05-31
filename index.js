ec73f1e26170309bc7df59c8aa4bc54487ee55dc
Jason Schmitt <schmitty890@gmail.com>
2021-05-31 15:30:09 -0400

    updating mongodb atlas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import routes from "./src/routes/crmRoutes";
 
 const app = express();
 
@@ -22,6 +23,31 @@ mongoose.connect(MONGODB_URI, {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// JWT setup
+app.use((req, res, next) => {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "JWT"
+  ) {
+    jsonwebtoken.verify(
+      req.headers.authorization.split(" ")[1],
+      "RESTFULAPIs",
+      (err, decode) => {
+        if (err) {
+          req.user = undefined;
+        }
+        req.user = decode;
+        next();
+      }
+    );
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+
+routes(app);
 // serving static files
 app.use(express.static("public"));
 
